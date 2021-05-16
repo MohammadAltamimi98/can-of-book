62693bd5c58db0dae78316b5659cc1fa7e444bae
Mohammad Altamimi <m98altamimi@gmail.com>
2021-05-16 14:58:43 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ import {
   Switch,
   Route
 } from "react-router-dom";
+import BestBooks from './BestBooks'
+import Login from 'Login'
+import Profile from './Profile'
 
 class App extends React.Component {
 
@@ -20,8 +23,13 @@ class App extends React.Component {
             <Switch>
               <Route exact path="/">
                 {/* TODO: if the user is logged in, render the `BestBooks` component, if they are not, render the `Login` component */}
+                <BestBooks/>
+                <Login/>
               </Route>
               {/* TODO: add a route with a path of '/profile' that renders a `Profile` component */}
+              <Route path="/profile">
+                <Profile/>
+                </Route>
             </Switch>
             <Footer />
           </IsLoadingAndError>
