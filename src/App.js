a63679a8b8d32738cafdb6bbdd1f1d92a654232f
Mohammad Altamimi <m98altamimi@gmail.com>
2021-05-17 15:08:17 +0300

    adding the card

--- src/App.js
+++ src/App.js
@@ -7,10 +7,11 @@ import {
   Switch,
   Route
 } from "react-router-dom";
-import LoginButton from './LoginButton';
+// import LoginButton from './LoginButton';
 import Profile from './Profile';
 import MyFavoriteBooks from './BestBooks';
 import { withAuth0 } from '@auth0/auth0-react';
+import Login from './Login';
 
 
 
@@ -27,7 +28,7 @@ class App extends React.Component {;
             <Switch>
               <Route exact path="/">
                 {/* TODO: if the user is logged in, render the `MyFavoriteBooks` component, if they are not, render the `Login` component */}
-                {this.props.auth0.isAuthenticated ?  <MyFavoriteBooks/> :   <LoginButton/>}
+                {this.props.auth0.isAuthenticated ?  <MyFavoriteBooks/> :  <Login/>}
                
                 </Route>
               {/* TODO: add a route with a path of '/profile' that renders a `Profile` component */}
