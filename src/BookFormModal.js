53991c88a7dce09538f32f351471ee39bb119a18
Mohammad Altamimi <m98altamimi@gmail.com>
2021-05-19 22:31:43 +0300

    hey

--- src/BookFormModal.js
+++ src/BookFormModal.js
@@ -7,18 +7,21 @@ class BookFormModal extends Component {
     render() {
         return (
             <div>
-                <form onSubmit = {(e)=>this.props.addBook(e)} >
+                <form onSubmit={(e) => this.props.addBook(e)} >
 
-                <label>Book Name</label>
-                <input type = 'text' onChange ={(e)=>this.props.updateBookName(e) }/>
-                <br />
-                <label>Book Description</label>
-                <input type = 'text' onChange ={(e)=>this.props.updateBookDescription(e)} />
-                <br />
-                <label>Book Status</label>
-                <input type = 'text' onChange ={(e)=>this.props.updateBookStatus(e) } />
-                <br />
-                <input type = 'submit' value ="Add Book" />
+                    <label>Book Name</label>
+                    <input type='text' onChange={(e) => this.props.updateBookName(e)} />
+                    <br />
+                    <label>Book Description</label>
+                    <input type='text' onChange={(e) => this.props.updateBookDescription(e)} />
+                    <br />
+                    <label>Book Status</label>
+                    <input type='text' onChange={(e) => this.props.updateBookStatus(e)} />
+                    <br />
+                    <button type="submit">
+                        ADD
+                    </button>
+
                 </form>
             </div>
         )
